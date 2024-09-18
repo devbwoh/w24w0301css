cb53375df62c9ede5723453952e508dfe03ce211
BWOH <bwoh@outlook.kr>
2024-09-18 18:40:49 +0900

    🎉 시작

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "w24w0301css"
---
+++ src/test/kotlin/kr/ac/kumoh/s20230000/w24w0301css/W24w0301cssApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh.s20230000.w24w0301css
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24w0301cssApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
